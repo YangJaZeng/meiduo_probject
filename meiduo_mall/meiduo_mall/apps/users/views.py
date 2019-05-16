278ec17b06c03b7c97eea8617d5ccc8090f03f82
杨 <xwp_fullstack@163.com>
2019-05-16 19:09:22 +0800

    第二天内容

---
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -0,0 +1,89 @@
+import re
+
+# from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import DatabaseError
+from django import http
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+
+from meiduo_mall.utils.response_code import RETCODE
+from .models import User
+
+
+class MobileCountView(View):
+    """判断手机号是否重复"""
+
+    def get(self, request, mobile):
+        count = User.objects.filter(mobile=mobile).count()
+        return http.JsonResponse({'code': RETCODE.OK,
+                                  'errmsg': 'ok',
+                                  'count': count})
+
+
+class UsernameCountView(View):
+    """判断用户名是否重复"""
+
+    def get(self, request, username):
+        count = User.objects.filter(username=username).count()
+        return http.JsonResponse({'code': RETCODE.OK,
+                                  'errmsg': 'ok',
+                                  'count': count})
+
+
+class RegisterView(View):
+    def get(self, request):
+        '''
+        定义一个接口,返回注册页面
+        :param request:
+        :return:
+        '''
+        return render(request, 'register.html')
+
+    def post(self, request):
+        """
+        实现用户注册
+        :param request: 请求对象
+        :return: 注册结果
+        """
+        # 接收参数
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get("password2")
+        mobile = request.POST.grt('mobile')
+        # TODO sms_code 还没有做
+        allow = request.POST.get('allow')
+
+        # 校验参数
+        # 判断参数是否齐全
+        if not all([username, password, password2, mobile, allow]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入5-20个字符的用户名')
+        # 判断密码是否是8-20个数字和字母
+        if not re.match(r'^[a-zA-Z0-9]{8,20}', password):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
+        # 判断两次密码是否一致
+        if password != password2:
+            return http.HttpResponseForbidden('两次密码不一致')
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 判断是否勾选用户协议
+        if allow != 'on':
+            return http.HttpResponseForbidden('请勾选用户协议')
+
+        # 保存注册数据
+        try:
+            # create_user函数可以加密密码
+            user = User.objects.create_user(username=username, password=password, mobile=mobile)
+        except DatabaseError:
+            return render(request, 'register.html', {'register_errmsg': '注册失败'})
+
+        login(request, user)
+        # 响应注册结果
+        return redirect(reverse('contents:index'))
+
+# Create your views here.
---
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -0,0 +1,30 @@
+import redis
+from django import http
+from django.shortcuts import render
+from django.views import View
+from django_redis import get_redis_connection
+
+from meiduo_mall.libs.captcha.captcha import captcha
+from verifications import const
+
+
+class ImageCode(View):
+    def get(self, request, uuid):
+        """
+
+        :param request:
+        :param uuid: 当前用户的唯一id
+        :return: image/jpg
+        """
+        #生成图片验证
+        text, image = captcha.generate_captcha()
+
+        #保存图片验证码
+        redis_conn = get_redis_connection('verify_code')
+
+        #图形验证码有效期，单位：秒
+        redis_conn.setex('img_%s' % uuid, const.IMAGE_COOE_REDIS_EXPIRES, text)
+
+        #响应图片验证码
+        return http.HttpResponse(image, content_type='image/jpg')
+# Create your views here.
