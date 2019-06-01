f5be0352ec7f2226b0bf68ceab12cb2f8ecdd956
杨 <xwp_fullstack@163.com>
2019-06-01 10:46:27 +0800

    订单

--- meiduo_mall/meiduo_mall/apps/orders/urls.py
+++ meiduo_mall/meiduo_mall/apps/orders/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^orders/commit/$', views.OrderCommitView.as_view()),
     # 订单提交成功过渡页面
     url(r'^orders/success/$', views.OrderSuccessView.as_view()),
+    # 我的订单
+    url(r'^orders/info/(?P<page_num>\d+)/$', views.UserOrderInfoView.as_view(), name='info'),
 ]
--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -2,6 +2,7 @@ import json
 from decimal import Decimal
 
 from django import http
+from django.core.paginator import Paginator, EmptyPage
 from django.db import transaction
 from django.shortcuts import render
 from django.utils import timezone
@@ -16,6 +17,48 @@ from utils.response_code import RETCODE
 import logging
 
 logger = logging.getLogger('django')
+
+
+class UserOrderInfoView(LoginRequiredMixin, View):
+    """我的订单"""
+
+    def get(self, request, page_num):
+        """提供我的订单页面"""
+        user = request.user
+        # 查询订单
+        orders = user.orderinfo_set.all().order_by("-create_time")
+        # 遍历所有订单
+        for order in orders:
+            # 绑定订单状态
+            order.status_name = OrderInfo.ORDER_STATUS_CHOICES[order.status - 1][1]
+            # 绑定支付方式
+            order.pay_method_name = OrderInfo.PAY_METHOD_CHOICES[order.pay_method - 1][1]
+            order.sku_list = []
+            # 查询订单商品
+            order_goods = order.skus.all()
+            # 遍历订单商品
+            for order_good in order_goods:
+                sku = order_good.sku
+                sku.count = order_good.count
+                sku.amount = sku.price * sku.count
+                order.sku_list.append(sku)
+
+        # 分页
+        page_num = int(page_num)
+        try:
+            paginator = Paginator(orders, 2)
+
+            page_orders = paginator.page(page_num)
+            total_page = paginator.num_pages
+        except EmptyPage:
+            return http.HttpResponseNotFound('订单不存在')
+
+        context = {
+            "page_orders": page_orders,
+            'total_page': total_page,
+            'page_num': page_num,
+        }
+        return render(request, "user_center_order.html", context)
 
 
 class OrderSuccessView(LoginRequiredMixin, View):
